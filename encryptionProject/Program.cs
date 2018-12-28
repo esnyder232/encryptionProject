8c410ede1143e7678c9194ba9207117996fc914b
esnyder232 <esnyder232@gmail.com>
2018-12-28 14:19:35 -0500

    commining var b from pc

--- encryptionProject/Program.cs
+++ encryptionProject/Program.cs
@@ -14,6 +14,7 @@ namespace encryptionProject
 
 
             string a = "";
+            string b = "no my change is better";
             
             Console.WriteLine("=== Encryption project done ===");
             Console.ReadLine();
