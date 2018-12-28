305608d48da80041695a3c75889c76cce20a62b7
esnyder232 <esnyder232@gmail.com>
2018-12-28 16:38:50 -0500

    adding old cipher stuff

--- encryptionProject/Program.cs
+++ encryptionProject/Program.cs
@@ -10,26 +10,90 @@ namespace encryptionProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("=== Encryption project started ===");
 
-            string hello = "world;";
+            Console.WriteLine("=== starting encryption program ===");
 
-            string a = "";
-            string b = "no my change is better";
-            string c = "my change here";
-            string d = "another change from pc";
-            string dhjfjdhfd = "";
-            
-            string again = "lkdjfdlkjfdkl";
-            
-            string notAgain = "uhoh";
-            string shouldnt = "overwrite";
-            string again2 = "shouldnt overwritekjsdhfjkdhf";
-            
-            Console.WriteLine("=== Encryption project done ===");
+            Dictionary<string, string> cipher = null;
+            Dictionary<string, string> reverseCipher = null;
 
-            string yayaya = "overlord2222333";
+            string key = "abcdefghijklmnopqrstuvwxyz ";
+            //string value = "zebrascdfghijklmnopqtuvwxy ";
+
+            string keyWord = "zebras";
+            string value = createValues(keyWord);
+
+            cipher = createCipher(key, value);
+            reverseCipher = createCipher(value, key);
+
+            string message = "hello world this is a secret message";
+
+            string cipherText = encode(message, cipher);
+            string decryptedMessage = encode(cipherText, reverseCipher);
+
+            Console.WriteLine("Original message: " + message);
+            Console.WriteLine("Encrypted message: " + cipherText);
+            Console.WriteLine("Decoded message: " + decryptedMessage);
+            Console.WriteLine("=== ending encryption program ===");
             Console.ReadLine();
+        }
+
+        static string encode(string message, Dictionary<string, string> cipher)
+        {
+            string ret = "";
+
+            List<string> plainText = new List<string>();
+
+            message = message.Replace(".", "").Replace(" ", "");
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                string plainChar = cipher[message[i].ToString()];
+                plainText.Add(plainChar);
+            }
+
+            string encyptedMessage = String.Join("", plainText.ToArray());
+
+            int groupCount = 5;
+            for (int i = encyptedMessage.Length / groupCount; i >= 0; i--)
+            {
+                encyptedMessage = encyptedMessage.Insert(i * groupCount, " ");
+            }
+
+            return encyptedMessage;
+        }
+
+        static string decode(string message, Dictionary<string, string> cipher)
+        {
+            string ret = "";
+
+            return ret;
+        }
+
+        static Dictionary<string, string> createCipher(string keys, string values)
+        {
+            Dictionary<string, string> cipher = new Dictionary<string, string>();
+            for (int i = 0; i < keys.Length; i++)
+            {
+                cipher.Add(keys[i].ToString(), values[i].ToString());
+            }
+            return cipher;
+        }
+
+        static string createValues(string keyword)
+        {
+            string ret = "";
+
+            string alphabet = "abcdefghijklmnopqrstuvwxyz ";
+            string remains = alphabet;
+
+            for (int i = 0; i < keyword.Length; i++)
+            {
+                remains = remains.Replace(keyword[i].ToString(), "");
+            }
+
+            ret = keyword + remains;
+
+            return ret;
         }
     }
 }
