a14e08eab66b0d8c2e2f5b910430b0d56ef2bc4b
esnyder232 <esnyder232@gmail.com>
2018-12-28 13:39:09 -0500

    Initial commit

---
+++ encryptionProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encryptionProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Encryption project started ===");
+
+
+
+            
+            Console.WriteLine("=== Encryption project done ===");
+            Console.ReadLine();
+        }
+    }
+}
